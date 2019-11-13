e9a9c05cddeb75179859d153f7e9f6c9b3c61cce
raiden <raam.balaji@gmail.com>
2019-11-13 17:27:37 +0530

    upgrading the material-ui version and adding comment expand minimize fn

--- app/Components/Comment.jsx
+++ app/Components/Comment.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
-import { Paper } from "@material-ui/core";
+import { Paper, Slider } from "@material-ui/core";
 
 const currentTime = moment();
 const createSubComments = comments => {
@@ -10,20 +10,45 @@ const createSubComments = comments => {
     ));
   }
 };
+
 const Comment = ({ comment }) => {
+  const [visibility, setVisibility] = useState(true);
   return (
     <div className="comment">
       <Paper className="comment-card">
-        <div className="comment-user-info">{`by ${comment.by} ${moment
-          .unix(comment.time)
-          .from(currentTime)}`}</div>
-        <div
-          className="comment-data"
-          dangerouslySetInnerHTML={{ __html: comment.text }}
-        ></div>
+        <div className="comment-user-info">
+          <span>
+            {`by ${comment.by} ${moment
+              .unix(comment.time)
+              .from(currentTime)}  `}
+          </span>
+          {!visibility && (
+            <span
+              className={"comment-expander mdi mdi-plus-box-outline"}
+              onClick={setVisibility.bind(this, true)}
+            >
+              {" "}
+            </span>
+          )}
+        </div>
+        {visibility && (
+          <div
+            className="comment-data"
+            dangerouslySetInnerHTML={{ __html: comment.text }}
+          ></div>
+        )}
       </Paper>
-
-      <div className="comment-children">{createSubComments(comment.kids)}</div>
+      {visibility && (
+        <div className={"comment-children-container"}>
+          <div
+            className={"comment-minimizer"}
+            onClick={setVisibility.bind(this, false)}
+          />
+          <div className="comment-children">
+            {createSubComments(comment.kids)}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
